74942599981bcce43152cc204c3a1d05fb6cffcc
Sheriff-Hoti <sherifhoti1235@hotmail.com>
2023-03-22 18:11:51 +0100

    Modified the initial migration scripts

--- SpringBootServer/security/src/main/resources/db/migration/V1__base_schema.sql
+++ SpringBootServer/security/src/main/resources/db/migration/V1__base_schema.sql
@@ -1,29 +1,8 @@
-CREATE SCHEMA jwt;
+CREATE SCHEMA IF NOT EXISTS jwt;
 
-CREATE TABLE IF NOT EXISTS jwt.confirmation_tokens
-(
-    id uuid NOT NULL,
-    confirmed_at timestamp(6) without time zone,
-    created_at timestamp(6) without time zone NOT NULL,
-    expires_at timestamp(6) without time zone NOT NULL,
-    token character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    app_user_id uuid NOT NULL,
-    CONSTRAINT confirmation_tokens_pkey PRIMARY KEY (id),
-    CONSTRAINT fki5j0v7b480rdn3v1g795cmwlb FOREIGN KEY (app_user_id)
-        REFERENCES public.users (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS jwt.roles
-(
-    id uuid NOT NULL,
-    name character varying(255) COLLATE pg_catalog."default",
-    CONSTRAINT roles_pkey PRIMARY KEY (id)
-)
-
-CREATE TABLE IF NOT EXISTS jwt.users
-(
+CREATE TABLE IF NOT EXISTS jwt.users(
     id uuid NOT NULL,
     email character varying(255) COLLATE pg_catalog."default",
     enabled boolean,
@@ -32,18 +11,39 @@ CREATE TABLE IF NOT EXISTS jwt.users
     locked boolean,
     password character varying(255) COLLATE pg_catalog."default",
     CONSTRAINT users_pkey PRIMARY KEY (id)
-)
+);
 
-CREATE TABLE IF NOT EXISTS jwt.users_roles
-(
+CREATE TABLE IF NOT EXISTS jwt.confirmation_tokens(
+    id uuid NOT NULL,
+    confirmed_at timestamp(6) without time zone,
+    created_at timestamp(6) without time zone NOT NULL,
+    expires_at timestamp(6) without time zone NOT NULL,
+    token character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    app_user_id uuid NOT NULL,
+    CONSTRAINT confirmation_tokens_pkey PRIMARY KEY (id),
+    CONSTRAINT fki5j0v7b480rdn3v1g795cmwlb FOREIGN KEY (app_user_id)
+        REFERENCES jwt.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
+CREATE TABLE IF NOT EXISTS jwt.roles(
+    id uuid NOT NULL,
+    name character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT roles_pkey PRIMARY KEY (id)
+);
+
+
+
+CREATE TABLE IF NOT EXISTS jwt.users_roles(
     app_user_id uuid NOT NULL,
     roles_id uuid NOT NULL,
     CONSTRAINT fka62j07k5mhgifpp955h37ponj FOREIGN KEY (roles_id)
-        REFERENCES public.roles (id) MATCH SIMPLE
+        REFERENCES jwt.roles (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
     CONSTRAINT fkar2y0lww0xn3x3aoqfg9qsgr5 FOREIGN KEY (app_user_id)
-        REFERENCES public.users (id) MATCH SIMPLE
+        REFERENCES jwt.users (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
-)
+);
---
+++ SpringBootServer/security/src/main/resources/db/migration/V2__add_role.sql
@@ -0,0 +1,4 @@
+INSERT INTO jwt.roles VALUES (uuid_generate_v4(),'SUPER_ADMIN');
+INSERT INTO jwt.roles VALUES (uuid_generate_v4(),'ADMIN');
+INSERT INTO jwt.roles VALUES (uuid_generate_v4(),'USER');
+INSERT INTO jwt.roles VALUES (uuid_generate_v4(),'SUBSCRIBED_USER');
