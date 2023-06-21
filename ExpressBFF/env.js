de632848339e9d4a95a0f8763e97aea82f9f7631
Sheriff-Hoti <sherifhoti1235@hotmail.com>
2023-06-21 22:06:15 +0200

    Added env.js

--- ExpressBFF/env.js
+++ ExpressBFF/env.js
@@ -4,7 +4,12 @@ dotenv.config();
 const env = {
     PORT: process.env.PORT,
     SECURE_SPRINGBOOT_PATH: `${process.env.SPRINGBOOT_SERVER_PATH}/api/v1/secured`,
-    UNSECURE_SPRINGBOOT_PATH: `${process.env.SPRINGBOOT_SERVER_PATH}/api/v1/auth`
+    UNSECURE_SPRINGBOOT_PATH: `${process.env.SPRINGBOOT_SERVER_PATH}/api/v1/auth`,
+    EXPRESS_SERVER_PATH: `${process.env.EXPRESS_SERVER_PATH}`,
+    JWT_REDIS_HOST: process.env.JWT_REDIS_HOST,
+    JWT_REDIS_PORT: process.env.JWT_REDIS_PORT,
+    JWT_REDIS_PASSWORD: process.env.JWT_REDIS_PASSWORD,
+    JWT_REDIS_URL: process.env.JWT_REDIS_URL
 }
 
 export default env;
